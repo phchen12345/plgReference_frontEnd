da994508aba3f0ae8848af060c8de637b73d8a97
Jasper Chen <tmsl9502@gmail.com>
2026-05-20 16:39:56 +0800

    暫時移除react query

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
-import { useState } from "react";
+// import { useState } from "react";
 
 export function Providers({ children }: Readonly<{ children: ReactNode }>) {
-  const [queryClient] = useState(() => new QueryClient());
+  // const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <ChakraProvider value={defaultSystem}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </ChakraProvider>
+    // <ChakraProvider value={defaultSystem}>
+    //   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    // </ChakraProvider>
+    <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
   );
 }
